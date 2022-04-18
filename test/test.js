e625bcd7da9d03f1a743d4c04252912bbb18b91b
Rizky Andi Jani <Rizky_A289@mitrais.com>
2022-04-18 16:25:14 +0700

    modiy index.js message

--- test/test.js
+++ test/test.js
@@ -3,10 +3,10 @@ var app = require('../index.js');
 
 describe('GET', function() {
  it('respond with hello world', function(done) {
- request(app).get('/').expect('hello world', done);
+ request(app).get('/').expect('hello Indonesia', done);
  });
 
  it('respond with hello user', function(done) {
-    request(app).get('/user').expect('hello user', done);
+    request(app).get('/user').expect('hello Indonesian User', done);
     });
 });
